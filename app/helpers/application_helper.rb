0dd259b21df05bf203a6a95ace1777eea3565ad3
Ankur Taxali <ataxali@gmail.com>
2026-02-05 22:53:04 -0500

    Build IGMenu Rails partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def ig_menu_css_size(value)
+    return nil if value.blank?
+    return "#{value}px" if value.is_a?(Numeric)
+
+    size = value.to_s.strip
+    return size if size.match?(/\A\d+(\.\d+)?(px|rem|em|vh|vw|%)\z/)
+
+    nil
+  end
+
+  def ig_menu_payload_data(payload)
+    return nil if payload.blank?
+    return payload.to_json if payload.is_a?(Hash)
+
+    payload.to_s
+  end
 end
