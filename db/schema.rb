229f9808d5c9bfa6f2acbc92ae83bf93d4159e2a
Ankur Taxali <ataxali@gmail.com>
2026-02-07 16:19:41 -0500

    Ensure asdf Ruby matches for db work

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_07_113000) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_07_170000) do
+  create_table "documents", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.string "title"
+    t.datetime "updated_at", null: false
+  end
+
   create_table "notes", force: :cascade do |t|
     t.integer "bottom"
     t.text "content"
